e6070279f55dc178081d793ab5174cf2fd4686a9
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-08-17 03:53:25 +0000

    complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,16 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# Employee.create(first_name: "Test3", last_name: "Test2", hourly_rate: 45)
+
+# Store.create(name: "Gas2", annual_revenue: "1", mens_apparel: true, womens_apparel: false)
+
+puts "Please enter a store name"
+store_name = gets.chomp
+new_store = Store.create(name: store_name)
+
+new_store.errors.messages.each do |key, value|
+  puts "#{key}: #{value}"
+end
+
+
